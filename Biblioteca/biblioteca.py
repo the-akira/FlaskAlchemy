12aa03a9ee02d6b37a5549e14e3507d5defe27f9
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-07-08 20:44:40 -0300

    Scripts atualizados

--- Biblioteca/biblioteca.py
+++ Biblioteca/biblioteca.py
@@ -27,22 +27,22 @@ db = SQLAlchemy(app)
 # >>> livro_1.autor
 
 class Autor(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	nome = db.Column(db.String(20), nullable=False)
-	sobrenome = db.Column(db.String(120), nullable=False)
-	livros = db.relationship('Livro', backref='autor', lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(20), nullable=False)
+    sobrenome = db.Column(db.String(120), nullable=False)
+    livros = db.relationship('Livro', backref='autor', lazy=True)
 
-	def __repr__(self):
-		return f'Autor("{self.nome}", "{self.sobrenome}")'
+    def __repr__(self):
+        return f'Autor("{self.nome}", "{self.sobrenome}")'
 
 class Livro(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	titulo = db.Column(db.String(100), nullable=False)
-	sumario = db.Column(db.String(250), nullable=False)
-	autor_id = db.Column(db.Integer, db.ForeignKey('autor.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    sumario = db.Column(db.String(250), nullable=False)
+    autor_id = db.Column(db.Integer, db.ForeignKey('autor.id'), nullable=False)
 
-	def __repr__(self):
-		return f'Livro("{self.titulo}")'
+    def __repr__(self):
+        return f'Livro("{self.titulo}")'
 
 if __name__ == '__main__':
     app.run(debug=True)
